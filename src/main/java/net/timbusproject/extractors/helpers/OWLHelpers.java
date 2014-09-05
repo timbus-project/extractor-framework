ce72e33d6c2e3b3d51ec50de63ff7372fe35c996
Rudolf Mayer <rudolf.mayer@gmail.com>
2014-09-05 23:34:08 +0200

    owl helpers

---
+++ src/main/java/net/timbusproject/extractors/helpers/OWLHelpers.java
@@ -0,0 +1,38 @@
+package net.timbusproject.extractors.helpers;
+
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLClassAssertionAxiom;
+import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLNamedIndividual;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+
+import uk.ac.manchester.cs.owl.owlapi.OWLNamedIndividualImpl;
+
+public class OWLHelpers {
+
+	/** Create an {@link OWLNamedIndividual} in the namespace of the given {@link OWLOntology} using the identifier */
+	public static OWLNamedIndividual createMachineNode(OWLOntology ontology, String identifier) {
+
+		OWLDataFactory dataFactory = ontology.getOWLOntologyManager().getOWLDataFactory();
+		IRI nodeIRI = IRI.create(ontology.getOntologyID().getOntologyIRI() + "#" + identifier);
+		OWLNamedIndividualImpl node = new OWLNamedIndividualImpl(nodeIRI);
+
+		// assign it to be of class Node
+		OWLClass nodeClass = dataFactory.getOWLClass(IRI.create("http://timbus.teco.edu/ontologies/DIO.owl", "Node"));
+		OWLClassAssertionAxiom classAssertion = dataFactory.getOWLClassAssertionAxiom(nodeClass, node);
+		ontology.getOWLOntologyManager().addAxiom(ontology, classAssertion);
+
+		return node;
+	}
+
+	public static void main(String[] args) throws OWLOntologyCreationException {
+		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+		OWLOntology ontology = manager.createOntology(IRI.create("http://timbusproject.net/test"));
+		OWLNamedIndividual machineNode = createMachineNode(ontology, new MachineID().getUUID());
+		System.out.println(machineNode);
+	}
+}
